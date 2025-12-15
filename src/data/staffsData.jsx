5aaa05b4f66617efe90b77b7669034150da39904
Milestone Paite <crepybozz@gmail.com>
2025-12-15 10:53:08 +0530

    removed gappasus

--- src/data/staffsData.jsx
+++ src/data/staffsData.jsx
@@ -1,10 +1,7 @@
-import teacher from "../assets/gappau.jpeg";
-import lab23 from "../assets/gapzi.jpeg";
+
 export const staffsData =    [
       { id: 1, state: "hs", position: "Principal", category: "teaching", name: "Sir. Principal", image: "https://i.pinimg.com/736x/36/32/39/363239ad815ea1bc2c059cfcd124437d.jpg", date: '01/01/2025 01:00 PM' },
       { id: 2, state: "hs", position: "Principal", category: "teaching", name: "Sir. Principal", image: "https://i.pinimg.com/736x/7c/b2/d5/7cb2d5436d224ad51b2ed1cde5948843.jpg", date: '01/01/2025 01:00 PM' },
-      { id: 3, state: "hs", position: "Guard", category: "teaching", name: "Gappau", image: teacher, date: '01/01/2025 01:00 PM' },
-      { id: 4, state: "hs", position: "Undertaker", category: "teaching", name: "Gapzi", image: lab23, date: '01/01/2025 01:00 PM' },
       { id: 5, state: "hs", position: "Principal", category: "teaching", name: "Sir. Principal", image: "https://i.pinimg.com/736x/36/32/39/363239ad815ea1bc2c059cfcd124437d.jpg", date: '01/01/2025 01:00 PM' },
 
       { id: 6, state: "hr", position: "Principal", category: "non-teaching", name: "Sir. Principal", image: "https://i.pinimg.com/736x/36/32/39/363239ad815ea1bc2c059cfcd124437d.jpg", date: '01/01/2025 01:00 PM' },
